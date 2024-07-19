8d6de776ae2848dfd0e476ca0b0ab45125d2ebb4
Ajay Kumar Shahu <1905922@kiit.ac.in>
2024-07-19 15:17:51 +0545

    crud

--- add.php
+++ add.php
@@ -14,18 +14,24 @@
             <label>Class</label>
             <select name="class">
                 <option value="" selected disabled>Select Class</option>
-                <option value="1">BCA</option>
-                <option value="2">BSC</option>
-                <option value="3">B.TECH</option>
+                <?php
+                $connect = mysqli_connect("localhost", "root", "", "crud") or die("connection failed!");
+                $sql = "SELECT * FROM studentclass";
+                $result = mysqli_query($connect, $sql) or die("Query Unsuccessful.");
+                while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                    <option value="<?php echo $row['cid']; ?>"> <?php echo $row['cname'] ?></option>
+                <?php } ?>
             </select>
         </div>
         <div class="form-group">
             <label>Phone</label>
             <input type="text" name="sphone" />
         </div>
-        <input class="submit" type="submit" value="Save"  />
+        <input class="submit" type="submit" value="Save" />
     </form>
 </div>
 </div>
 </body>
+
 </html>
--- edit.php
+++ edit.php
@@ -2,32 +2,54 @@
 
 <div id="main-content">
     <h2>Update Record</h2>
-    <form class="post-form" action="updatedata.php" method="post">
-      <div class="form-group">
-          <label>Name</label>
-          <input type="hidden" name="sid" value=""/>
-          <input type="text" name="sname" value=""/>
-      </div>
-      <div class="form-group">
-          <label>Address</label>
-          <input type="text" name="saddress" value=""/>
-      </div>
-      <div class="form-group">
-          <label>Class</label>
-          <select name="sclass">
-              <option value="" selected disabled>Select Class</option>
-              <option value="1">BCA</option>
-              <option value="2">BSC</option>
-              <option value="3">B.TECH</option>
-          </select>
-      </div>
-      <div class="form-group">
-          <label>Phone</label>
-          <input type="text" name="sphone" value=""/>
-      </div>
-      <input class="submit" type="submit" value="Update"/>
-    </form>
+    <?php
+    $connect = mysqli_connect("localhost", "root", "", "crud") or die("connection failed!");
+    $stu_id = $_GET['id'];
+    $sql = "SELECT * FROM student WHERE sid = {$stu_id}";
+    $result = mysqli_query($connect, $sql) or die("Query Unsuccessful.");
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+
+            <form class="post-form" action="updatedata.php" method="post">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="hidden" name="sid" value="<?php echo $row['sid']; ?>" />
+                    <input type="text" name="sname" value="<?php echo $row['sname']; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Address</label>
+                    <input type="text" name="saddress" value="<?php echo $row['saddress']; ?>" />
+                </div>
+                <div class="form-group">
+                    <label>Class</label>
+                    <?php
+                    $sql1 = "SELECT * FROM studentclass";
+                    $result1 = mysqli_query($connect, $sql1) or die("Query Unsuccessful.");
+
+                    if (mysqli_num_rows($result1) > 0) {
+                        echo '<select name="sclass">
+                        <option value="" selected disabled>Select Class</option>'
+                     while ($row1 = mysqli_fetch_assoc($result1)) {
+                    
+                        echo '<option value="1">BCA</option>';
+                     }
+                   echo </select>;
+                    }
+                    ?>
+                </div>
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input type="text" name="sphone" value="<?php echo $row['sphone']; ?>" />
+                </div>
+                <input class="submit" type="submit" value="Update" />
+            </form>
+    <?php }
+    }
+    ?>
 </div>
 </div>
 </body>
+
 </html>
